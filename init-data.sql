91c64399ea3ae0ea5ad5269d71b497eebefb5b37
JulieNonnon <julie.nonnon@gmail.com>
2024-05-14 22:56:30 +0200

    feature/added init-data.sql, product table updated

---
+++ init-data.sql
@@ -0,0 +1,68 @@
+INSERT INTO product (title, image, small_description, long_description, price, pitch, feature1, feature2, feature3)
+VALUES
+    (
+      'Coeur de boeuf', 
+      'assets/img/product-coeur-de-boeuf.png',
+      'Sachet individuel d"une quinzaine de graines.', 
+      'Une variété qui séduira les amateurs de tomates par sa forme distinctive et son goût exceptionnel. Ces tomates sont caractérisées par leur grande taille, leur forme côtelée et leur chair charnue, offrant une expérience gustative riche et savoureuse.', 
+      4,
+      'Faites de la Tomate Cœur de Bœuf le joyau de votre jardin et savourez des récoltes abondantes de ces délicieuses tomates. Transformez vos plats en créations culinaires irrésistibles grâce à cette variété exceptionnelle.',
+      'Les tomates Cœur de Bœuf se distinguent par leur forme généreuse et arrondie, évoquant celle d"un cœur. La chair de ces tomates est juteuse, dense et pleine de saveurs, idéale pour des salades gourmandes, des sauces ou des sandwiches.',
+      'Une douceur naturelle équilibrée avec une légère acidité, créant une combinaison de saveurs inoubliable. Chaque bouchée libère un arôme frais et enivrant qui capture l"essence même des tomates cultivées avec soin.',
+      'Nos graines de tomates Cœur de Bœuf sont sélectionnées avec soin pour garantir des plants vigoureux et productifs. Nous nous engageons à promouvoir des pratiques agricoles durables pour vous offrir des produits de la plus haute qualité.'
+    ),
+    ( 
+      'Beauty Queen', 
+      'assets/img/product-beauty-queen.png',
+      'Sachet individuel d"une quinzaine de graines.', 
+      'Une variété qui porte bien son nom grâce à sa beauté éclatante et son goût exquis. Ces tomates sont un véritable régal pour les yeux et les papilles, idéales pour ceux qui recherchent une touche de sophistication dans leur jardin et leurs plats.', 
+      5,
+      'Transformez votre jardin en un royaume de beauté en cultivant la Tomate Beauty Queen et laissez-vous enchanter par sa splendeur et ses saveurs exquises. Ajoutez une touche d"élégance à vos repas et émerveillez-vous devant la majesté de cette variété de tomate.',
+      'La Tomate Beauty Queen séduit par sa peau lisse et brillante, d"un rouge profond parsemé de striures dorées, créant un spectacle visuel irrésistible. Ces tomates ont une forme légèrement aplatie et côtelée, évoquant la silhouette gracieuse d"une reine, ajoutant une dimension esthétique à votre jardin.',
+      'Une douceur subtile équilibrée par une légère acidité, créant une harmonie de saveurs parfaitement équilibrée. Chaque tomate dégage un arôme délicat qui embaume le jardin et annonce la promesse de délices culinaires à venir.',
+      'Semences de Qualité : Nos graines de Tomate Beauty Queen sont sélectionnées avec soin pour assurer des plants vigoureux et productifs, garantissant une récolte abondante. Engagement envers la Qualité : Nous nous engageons à vous offrir des produits de la plus haute qualité, cultivés avec passion et souci du détail.' 
+    ),
+    ( 
+      'Noire de crimée', 
+      'assets/img/product-noire-de-crimee.png',
+      'Sachet individuel d"une quinzaine de graines.', 
+      'Une variété unique réputée pour son aspect sombre et sa saveur incomparable. Originaire de la région de Crimée, cette tomate est un véritable trésor culinaire, parfait pour ceux qui recherchent une expérience gustative riche et exotique.', 
+      3,
+      'Ajoutez une touche d"exotisme à votre jardin en cultivant la Tomate Noire de Crimée et savourez des récoltes abondantes de ces précieuses tomates. Laissez-vous séduire par sa beauté sombre et ses saveurs uniques qui évoquent les mystères de la Crimée.',
+      'La Tomate Noire de Crimée se distingue par sa couleur profonde, presque pourpre, qui contraste magnifiquement avec sa chair juteuse. Ces tomates ont une forme arrondie à légèrement aplatie et une texture charnue, offrant une expérience en bouche à la fois tendre et juteuse.',
+      'La saveur de la Tomate Noire de Crimée est un mélange enivrant de douceur sucrée et d"acidité subtile, avec des notes terreuses qui ajoutent une profondeur de caractère. Un arôme envoûtant, évoquant les terres fertiles de la Crimée où elle est cultivée depuis des générations.',
+      'Nos graines sont sélectionnées avec rigueur pour assurer des plants robustes et productifs. Nous honorons les méthodes traditionnelles de culture pour préserver l"authenticité et la qualité de cette variété emblématique.'
+    ),
+    ( 
+      'Ananas', 
+      'assets/img/product-ananas.png',
+      'Sachet individuel d"une quinzaine de graines.', 
+      'Une variété captivante réputée pour sa chair juteuse et son goût délicieusement sucré. Originaire des tropiques, cette tomate est un véritable trésor pour les amateurs de saveurs exotiques et les passionnés de jardinage.', 
+      6,
+      'Transformez votre jardin en un paradis tropical en cultivant la Tomate Ananas et laissez-vous envoûter par sa beauté et ses saveurs exotiques. Ajoutez une touche de soleil à vos repas et savourez l"éclat de cette variété de tomate exceptionnelle.',
+      'La Tomate Ananas séduit par sa peau rayée jaune doré et rouge, évoquant les couleurs vives des fruits tropicaux mûrs à point. Ces tomates ont une chair juteuse et fondante, offrant une expérience en bouche à la fois douce et rafraîchissante.',
+      'Une douceur sucrée inégalée, rappelant les arômes envoûtants des fruits tropicaux mûrs sous le soleil. Chaque bouchée révèle des nuances subtiles de saveurs tropicales, évoquant des souvenirs de vacances paradisiaques.',
+      'Nos graines sont sélectionnées avec soin pour garantir des plants vigoureux et productifs, prêts à prospérer dans votre jardin. Nous nous engageons à vous offrir des produits de la plus haute qualité, cultivés avec passion et dévouement.'
+    ),
+    ( 
+      'Green Zebra', 
+      'assets/img/product-green-zebra.png',
+      'Sachet individuel d"une quinzaine de graines.', 
+      'Une variété unique qui marie la fraîcheur de la couleur verte avec la douceur juteuse des tomates. Ces tomates sont un régal visuel et gustatif, idéales pour apporter une touche d"originalité à votre jardin et à vos plats.', 
+      2,
+      'Transformez votre jardin en une oasis de fraîcheur en cultivant la Tomate Green Zebra et laissez-vous envoûter par sa beauté et ses saveurs uniques. Ajoutez une touche exotique à vos plats et émerveillez-vous devant la vivacité de cette variété de tomate.',
+      'La Tomate Green Zebra se distingue par sa peau rayée vert clair et jaune, créant un contraste saisissant qui attire immédiatement l"attention. Ces tomates ont une chair ferme et juteuse, offrant une expérience en bouche à la fois croquante et succulente.',
+      'Une combinaison délicate de douceur sucrée et d"acidité rafraîchissante, créant une symphonie de saveurs équilibrée. Un parfum frais et vivifiant qui évoque les chaudes journées d"été et les promenades dans le jardin.',
+      'Nos graines sont soigneusement sélectionnées pour garantir des plants vigoureux et productifs, prêts à prospérer dans votre jardin. Nous nous engageons à promouvoir des pratiques agricoles durables pour assurer la santé de la planète et la qualité de nos produits.'
+    ),
+    ( 
+      'White Beauty', 
+      'assets/img/product-white-beauty.png',
+      'Sachet individuel d"une quinzaine de graines.', 
+      'Une variété unique qui marie la douceur délicate de la couleur blanche avec une saveur rafraîchissante et succulente. Ces tomates sont un régal pour les yeux et les papilles, idéales pour apporter une touche de sophistication à votre jardin et à vos plats.', 
+      66.55,
+      'Transformez votre jardin en un sanctuaire de beauté en cultivant la Tomate White Beauty et laissez-vous envoûter par sa splendeur et ses saveurs uniques. Ajoutez une touche d"élégance à vos repas et émerveillez-vous devant la pureté de cette variété de tomate.',
+      'La Tomate White Beauty séduit par sa peau lisse et nacrée, d"un blanc pur et lumineux qui évoque la pureté et l"innocence. Ces tomates ont une chair tendre et juteuse, offrant une expérience en bouche à la fois douce et rafraîchissante.',
+      'Une douceur subtile équilibrée par une légère acidité, créant une symphonie de saveurs délicates et harmonieuses. Chaque bouchée libère un parfum frais et envoûtant, évoquant la pureté et la fraîcheur des jardins en fleurs.',
+      'Nos graines sont sélectionnées avec soin pour garantir des plants vigoureux et productifs, prêts à prospérer dans votre jardin. Nous nous engageons à vous offrir des produits de la plus haute qualité, cultivés avec passion et dévouement.'
+    );
